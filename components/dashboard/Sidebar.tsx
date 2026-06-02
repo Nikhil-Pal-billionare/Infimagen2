6913f7abb3a6ff61a35911c6835704fe5ec65343
Nikhil-Pal-billionare <support@foxgen.in>
2026-06-02 09:14:07 +0530

    Update Sidebar.tsx

--- components/dashboard/Sidebar.tsx
+++ components/dashboard/Sidebar.tsx
@@ -12,7 +12,7 @@ import {
   LogOut
 } from "lucide-react";
 import Link from "next/link";
-import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
+import { createBrowserClient } from '@supabase/ssr';
 import { useRouter } from 'next/navigation';
 
 /* =========================
@@ -47,7 +47,10 @@ export default function Sidebar({
   onOpenMobile,
   onCloseMobile,
 }: SidebarProps) {
-  const supabase = createClientComponentClient();
+  const supabase = createBrowserClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+);
   const router = useRouter();
 
   const handleLogout = async () => {
