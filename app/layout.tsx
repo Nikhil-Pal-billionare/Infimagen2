60d8621086d349fe4323957fe96dba6a909c3b16
Nikhil-Pal-billionare <support@foxgen.in>
2026-06-14 14:39:26 +0530

    Add Clarity tracking script to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,13 @@ return (
     <html lang="en">
       <body className="min-h-screen bg-[#0D0D0D] text-white">
         {children}
+        <script type="text/javascript">
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "wuljhk1k5o");
+        </script>
       </body>
     </html>
   );
